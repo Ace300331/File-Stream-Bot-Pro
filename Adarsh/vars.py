a6590294f92c70662319d0944570210238db9b95
Ace300331 <kujoora923@gmail.com>
2025-06-26 12:39:31 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,21 +9,21 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '11973721'))
-    API_HASH = str(getenv('API_HASH', '5264bf4663e9159565603522f58d3c18'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN','5990559544:AAHUhUFy54tDKGubpU_dMSpSGTRthmgImAw'))
+    API_ID = int(getenv('API_ID', '23790461'))
+    API_HASH = str(getenv('API_HASH', '2d0a7fb85f06246e93948bf7afc05fb3'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN','7846781495:AAG4kZ2uiX96e0-_7n1IW-4-GkJ8ZKuBMgg'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001871766752'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002786845877'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'starstreamer.hop.sh'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1391556668").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "929048904").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     PORT = int(environ.get("PORT", 8080))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', '@TG_Karthik'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Amit_Shadow'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME', 'file-stream-bot-pro'))
@@ -36,10 +36,10 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://KarthikMovies:KarthikUK007@cluster0.4l5byki.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Star_Bots_Tamil'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Rax:amit7001@cluster0.cintttk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Ace_Files'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001296894100")).split())) 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001821439025"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002786845877"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
     KEEP_ALIVE = str(environ.get("KEEP_ALIVE", "1").lower()) in  ("1", "true", "t", "yes", "y")
     DEBUG = str(environ.get("DEBUG", "0").lower()) in ("1", "true", "t", "yes", "y")
